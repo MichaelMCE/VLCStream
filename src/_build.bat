90f301392278bf0ee9cb8a3156f32efa24b6a02c
MichaelMcE <mapei_@hotmail.com>
2025-06-04 00:49:33 +0100

    update compile script

--- src/_build.bat
+++ src/_build.bat
@@ -7,7 +7,8 @@ call make xcpy
 call make -j10 vlcstream.exe
 rename vlcstream.exe _vlcstream.exe
 
-call copy _vlcstream.exe "C:\Program Files (x86)\VLC\_vlcstream.exe" /y
-call copy _vlcstream.exe "M:\RamDiskTemp\vlc\_vlcstream.exe" /y
+rem call copy _vlcstream.exe "C:\Program Files (x86)\VLC\_vlcstream.exe" /y
+rem call copy _vlcstream.exe "C:\Program Files (x86)\vlcStream32\_vlcstream.exe" /y
+call copy _vlcstream.exe "C:\Program Files (x86)\vlc-2.2.0\_vlcstream.exe" /y
 
 
