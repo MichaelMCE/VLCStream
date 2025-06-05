95ae9f552668afe560a5b1548ccbc822518852e6
MichaelMcE <mapei_@hotmail.com>
2025-06-05 23:13:26 +0100

    Update version number

--- src/ui.h
+++ src/ui.h
@@ -25,7 +25,7 @@
 #define _UI_H_
 
 #define PLAYER_NAME				"VLCStream"
-#define PLAYER_DATE				"05NOV2019"	/* __DATE__ */
+#define PLAYER_DATE				"05JUNE2025"	/* __DATE__ */
 #define PLAYER_VERSION			PLAYER_NAME "-" PLAYER_DATE
 
 #define VCHROMA					"RV32"			/* Select VLC output chroma */
